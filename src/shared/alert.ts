00277a20a7804604c9c15dca22b2d8d6e72f56fb
Tharlei Aleixo <tharlei_aleixo@hotmail.com>
2022-09-25 23:28:47 -0300

    feat: add libs and shared classes to help

---
+++ src/shared/alert.ts
@@ -0,0 +1,37 @@
+import Swal, { SweetAlertResult } from "sweetalert2";
+
+export class Alert {
+  static toastError(title: string): void {
+    Swal.fire({
+      title,
+      icon: "error",
+      toast: true,
+      timer: 3000,
+      timerProgressBar: true,
+      position: "top-end",
+      showConfirmButton: false,
+    });
+  }
+
+  static toastSuccess(title: string): void {
+    Swal.fire({
+      title,
+      icon: "success",
+      toast: true,
+      timer: 3000,
+      timerProgressBar: true,
+      position: "top-end",
+      showConfirmButton: false,
+    });
+  }
+
+  static async confirmMessage(text: string): Promise<SweetAlertResult> {
+    return await Swal.fire({
+      text,
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonText: "Confirmar",
+      cancelButtonText: "Cancelar",
+    });
+  }
+}
